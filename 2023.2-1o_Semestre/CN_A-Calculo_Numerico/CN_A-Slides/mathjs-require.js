55ee219aabc209d5c5df087572185b6fd79ac10e
Felipe Pinto <32753781+Felipe-9@users.noreply.github.com>
2023-12-03 01:30:32 +0000

    file dump

---
+++ 2023.2-1o_Semestre/CN_A-Calculo_Numerico/CN_A-Slides/mathjs-require.js
@@ -0,0 +1,5 @@
+var math = require("/opt/homebrew/lib/node_modules/mathjs/lib/cjs/index.js")
+// U * x = b
+math.usolveAll(U, b)
+// Raizes
+math.polynomialRoot(constant, linearCoeff, quadraticCoeff, cubicCoeff)
